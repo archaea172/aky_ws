65c7c38fcaf7d324761b3ca59679c3b053420d89
aky <akiyugata271@gmail.com>
2026-04-26 16:44:02 +0000

    initial commit

---
+++ src/irc_table/launch/irc_table.launch.py
@@ -0,0 +1,61 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, AppendEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+
+def generate_launch_description():
+    # for spawn turtlebot3
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    world = os.path.join(
+        get_package_share_directory('irc_table'),
+        'worlds',
+        'irc_table.sdf'
+    )
+
+    gz_launch_path = PathJoinSubstitution([ros_gz_sim, 'launch', 'gz_sim.launch.py'])
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': ['-r -s -v2 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-g -v2 ', 'on_exit_shutdown': 'true'}.items()
+    )
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            os.path.join(
+                get_package_share_directory('irc_table'),
+                'models'))
+
+    spawn_balls_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('irc_table'),
+                'launch',
+                'spawn_random_balls.launch.py'
+            )
+        )
+    )
+
+    ld = LaunchDescription()
+
+    # Add the commands to the launch description
+    ld.add_action(set_env_vars_resources)
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(spawn_balls_cmd)
+
+    return ld
+
