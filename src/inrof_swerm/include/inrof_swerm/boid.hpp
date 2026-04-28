e3ca0d202ba629102250d65040ffd5efb285473c
aky <akiyugata271@gmail.com>
2026-04-28 06:49:31 +0000

    boid_nodeのCMakeLists.txtを更新し、依存関係の追加とインストール設定を追加。boid.hppおよびboid.cppに新しいメンバー変数とコールバック関数を追加し、パラメータの宣言とタイマーを設定。

--- src/inrof_swerm/include/inrof_swerm/boid.hpp
+++ src/inrof_swerm/include/inrof_swerm/boid.hpp
@@ -3,8 +3,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/node.hpp"
 
+#include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "geometry_msgs/msg/pose.hpp"
 
 #include <Eigen/Dense>
 
@@ -14,6 +14,16 @@ class boid_node
 public:
     boid_node();
 private:
+    void odom_callback(int id, nav_msgs::msg::Odometry::ConstSharedPtr rxdata);
+    void control_callback();
+
     int boid_num_;
-    std::vector<geometry_msgs::msg::Pose> pose_;
+    double max_vel_;
+    Eigen::MatrixXd pos_matrix_;
+    Eigen::MatrixXd vel_matrix_;
+    
+    std::vector<nav_msgs::msg::Odometry> odoms_;
+    std::vector<rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr> cmd_vel_publishers_;
+    std::vector<rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr> odom_subscribers_;
+    rclcpp::TimerBase::SharedPtr control_timer_;
 };
--- src/inrof_swerm/src/boid.cpp
+++ src/inrof_swerm/src/boid.cpp
@@ -1,6 +1,30 @@
 #include "boid.hpp"
 
+using namespace std::chrono_literals;
+
 boid_node::boid_node()
 : rclcpp::Node("boid")
 {
+    this->declare_parameter<double>("boid_num");
+    this->boid_num_ = this->get_parameter("boid_num").as_double();
+
+    rclcpp::QoS device = rclcpp::QoS(rclcpp::KeepLast(10))
+        .reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE)
+        .durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+
+    this->cmd_vel_publishers_.reserve(this->boid_num_);
+    for (int i = 0; i < this->boid_num_; ++i)
+    this->cmd_vel_publishers_.push_back(
+        this->create_publisher<geometry_msgs::msg::Twist>(
+            "robot_" + std::to_string(i) + "/cmd_vel",
+            device
+        )
+    );
+
+    this->control_timer_ = rclcpp::create_timer(
+        this,
+        this->get_clock(),
+        10ms,
+        std::bind(&boid_node::control_callback, this)
+    );
 }
