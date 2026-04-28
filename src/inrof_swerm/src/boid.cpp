9c94628d385abbe04f03aebde1c05c9d6f3a3041
aky <akiyugata271@gmail.com>
2026-04-28 07:24:28 +0000

    boid_nodeにEigen3とOpenMPの依存関係を追加し、CMakeLists.txtを更新。update_velメソッドを追加。

--- src/inrof_swerm/src/boid.cpp
+++ src/inrof_swerm/src/boid.cpp
@@ -72,3 +72,17 @@ void boid_node::control_callback()
         this->cmd_vel_publishers_[i]->publish(txdata);
     }
 }
+
+Eigen::MatrixXd boid_node::update_vel()
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    std::shared_ptr<boid_node> node = std::make_shared<boid_node>();
+    rclcpp::spin(node->get_node_base_interface());
+    rclcpp::shutdown();
+    return 0;
+}
