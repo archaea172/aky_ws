4b6f58f731ddf6ed6ddd2a2282863e24adc3ca3d
aky <akiyugata271@gmail.com>
2026-05-04 17:33:52 +0000

    sheep_main.cppを新規作成し、sheep_nodeの初期化とスピン処理を実装

--- src/inrof_swerm/src/sheep_main.cpp
+++ src/inrof_swerm/src/sheep_main.cpp
@@ -1,9 +1,9 @@
-#include "ros/boid_node.hpp"
+#include "ros/sheep_node.hpp"
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    std::shared_ptr<boid_node> node = std::make_shared<boid_node>();
+    std::shared_ptr<sheep_node> node = std::make_shared<sheep_node>();
     rclcpp::spin(node->get_node_base_interface());
     rclcpp::shutdown();
     return 0;
