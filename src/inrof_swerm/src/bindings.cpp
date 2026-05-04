a981474629aacf65647dbf5a538c6acdef6ff8dc
aky <akiyugata271@gmail.com>
2026-05-04 18:52:39 +0000

    SheepCoreクラスを追加し、犬回避力を持つボイド群の速度更新機能を実装

--- src/inrof_swerm/src/bindings.cpp
+++ src/inrof_swerm/src/bindings.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
 #include <pybind11/stl.h>
 
 #include "core/boid_core.hpp"
@@ -20,4 +21,39 @@ PYBIND11_MODULE(inrof_swerm_cpp, m)
         .def_readwrite("k_gravity", &BoidPrams::k_gravity, "Cohesion force gain.")
         .def_readwrite("Ir", &BoidPrams::Ir, "Interaction radius.")
         .def_readwrite("Ir_min", &BoidPrams::Ir_min, "Minimum interaction radius.");
+
+    pybind11::class_<BoidCore>(
+        m, "BoidCore",
+        "Core boid swarm velocity updater.")
+        .def(pybind11::init<const BoidPrams&>(), pybind11::arg("params"))
+        .def(
+            "update_vels",
+            &BoidCore::update_vels,
+            pybind11::arg("pos_matrix"),
+            pybind11::arg("vel_matrix"),
+            pybind11::call_guard<pybind11::gil_scoped_release>());
+
+    pybind11::class_<FollowerCore, BoidCore>(
+        m, "FollowerCore",
+        "Boid swarm velocity updater with leader-following force.")
+        .def(pybind11::init<const BoidPrams&, double>(), pybind11::arg("params"), pybind11::arg("k_follow"))
+        .def(
+            "update_vels",
+            &FollowerCore::update_vels,
+            pybind11::arg("pos_matrix"),
+            pybind11::arg("vel_matrix"),
+            pybind11::arg("leader_pos"),
+            pybind11::call_guard<pybind11::gil_scoped_release>());
+
+    pybind11::class_<SheepCore, BoidCore>(
+        m, "SheepCore",
+        "Boid swarm velocity updater with dog-avoidance force.")
+        .def(pybind11::init<const BoidPrams&, double>(), pybind11::arg("params"), pybind11::arg("k_run"))
+        .def(
+            "update_vels",
+            &SheepCore::update_vels,
+            pybind11::arg("pos_matrix"),
+            pybind11::arg("vel_matrix"),
+            pybind11::arg("dog_pos"),
+            pybind11::call_guard<pybind11::gil_scoped_release>());
 }
