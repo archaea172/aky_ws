d204a542dfcd5fd8bf47c242f198c16cefcff767
aky <akiyugata271@gmail.com>
2026-05-13 12:27:03 +0000

    lifecycle_serverのヘッダーファイルと実装ファイルを新規作成

---
+++ src/inrof_bt/include/inrof_bt/lifecycle_server.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/node.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+#include "lifecycle_msgs/srv/change_state.hpp"
+#include "lifecycle_msgs/srv/get_state.hpp"
