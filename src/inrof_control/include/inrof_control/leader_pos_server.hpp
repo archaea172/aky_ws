4876a8e08f66e723e3abc61094f7f8c5dd5bb217
aky <akiyugata271@gmail.com>
2026-05-15 04:10:17 +0000

    LeaderPosServerクラスを新規作成し、リーダーパスのアクションサーバーを実装

---
+++ src/inrof_control/include/inrof_control/leader_pos_server.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/node.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+#include "swerm_msgs/srv/leader_path.hpp"
+#include "swerm_msgs/action/leader_pos.hpp"
+
+class LeaderPosServer
+: public rclcpp::Node
+{
+public:
+    LeaderPosServer();
+
+private:
+    using GoalHandleLeaderPos = rclcpp_action::ServerGoalHandle<swerm_msgs::action::LeaderPos>;
+
+    rclcpp_action::GoalResponse handle_goal(
+        const rclcpp_action::GoalUUID &,
+        std::shared_ptr<const swerm_msgs::action::LeaderPos::Goal> goal
+    );
+    rclcpp_action::CancelResponse handle_cancel(
+        const std::shared_ptr<GoalHandleLeaderPos> goal_handle
+    );
+    void handle_accepted(const std::shared_ptr<GoalHandleLeaderPos> goal_handle);
+    void execute(const std::shared_ptr<GoalHandleLeaderPos> goal_handle);
+
+    rclcpp_action::Server<swerm_msgs::action::LeaderPos>::SharedPtr action_server_;
+};
---
+++ src/inrof_control/src/leader_pos_server.cpp
@@ -0,0 +1,1 @@
+#include "leader_pos_server.hpp"
