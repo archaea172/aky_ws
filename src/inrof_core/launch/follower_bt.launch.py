7420ed9dfdbbbc719c03591bcefefd957c235349
aky <akiyugata271@gmail.com>
2026-05-17 16:23:23 +0000

    follower_bt.launch.pyにマップサーバーのライフサイクルノードとイベントハンドラーを追加

--- src/inrof_core/launch/follower_bt.launch.py
+++ src/inrof_core/launch/follower_bt.launch.py
@@ -1,23 +1,31 @@
 import os
 
+import launch
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node, LifecycleNode
+from launch_ros.events.lifecycle import ChangeState
+from launch.event_handlers import OnProcessStart
+from launch_ros.event_handlers import OnStateTransition
+from launch.actions import RegisterEventHandler, EmitEvent
 
+import lifecycle_msgs.msg
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     ld = LaunchDescription()
 
+    # Load parameters for follower and map server
     follower_param = os.path.join(
         get_package_share_directory('inrof_swerm'),
         'config',
         'follower.yaml'
     )
+    map_yaml = os.path.join(
+        get_package_share_directory('inrof_swerm'),
+        'map',
+        'irc.yaml'
+    )
+
     follower_node = Node(
         package='inrof_swerm',
         executable='follower_node',
@@ -25,6 +33,45 @@ def generate_launch_description():
         parameters=[follower_param]
     )
     ld.add_action(follower_node)
+
+    map_server_node = LifecycleNode(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        namespace='',
+        parameters=[{'yaml_filename': map_yaml}]
+    )
+    map_configure_event_handler = RegisterEventHandler(
+        OnProcessStart(
+            target_action=map_server_node,
+            on_start=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=launch.events.matches_action(map_server_node),
+                        transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+                    )
+                )
+            ]
+        )
+    )
+    map_activate_event_handler = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=map_server_node,
+            start_state='configuring',
+            goal_state='inactive',
+            entities=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=launch.events.matches_action(map_server_node),
+                        transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
+                    )
+                )
+            ]
+        )
+    )
+    ld.add_action(map_server_node)
+    ld.add_action(map_configure_event_handler)
+    ld.add_action(map_activate_event_handler)
 
     leader_pos_server_node = Node(
         package='inrof_control',
