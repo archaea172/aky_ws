2f945a42ac65188018c621a435cecda21ef38404
aky <akiyugata271@gmail.com>
2026-05-30 15:04:22 +0000

    LeaderRLクラスを追加し、複数ロボットのオドメトリ情報を管理する機能を実装

---
+++ src/inrof_control/src/inrof_control/leader_rl.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionServer
+from nav_msgs.msg import Odometry
+from swerm_msgs.action import LeaderPos
+
+class LeaderRL(Node):
+    def __init__(self):
+        super().__init__("leader_rl")
+        self.leader_publisher_ = self.create_publisher(
+            Odometry,
+            "leader/odometry",
+            10
+        )
+        self.robot_num = 5
+        self.robot_odometry = [None] * self.robot_num
+        self.odom_subscribers = [
+            self.create_subscription(
+                Odometry,
+                f"robot_{i}/odometry",
+                lambda msg, robot_id=i: self.odom_callback(robot_id, msg),
+                10
+            )
+            for i in range(self.robot_num)
+        ]
+
+    def odom_callback(self, robot_id, msg):
+        self.robot_odometry[robot_id] = msg
