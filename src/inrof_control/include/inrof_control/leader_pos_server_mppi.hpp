c49e81a7d8436cebe96cb9eaf5aa1613585dee2c
aky <akiyugata271@gmail.com>
2026-06-23 00:16:03 +0900

    LeaderPosServerクラスを新規作成し、リーダーの位置を管理するアクションサーバーを実装

---
+++ src/inrof_control/include/inrof_control/leader_pos_server_mppi.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/node.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+#include "swerm_msgs/srv/leader_path.hpp"
+#include "swerm_msgs/action/leader_pos.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+#include "mppi_swerm.hpp"
+
+class LeaderPosServer
+: public rclcpp::Node
+{
+public:
+    LeaderPosServer();
+
+private:
+    using GoalHandleLeaderPos = rclcpp_action::ServerGoalHandle<swerm_msgs::action::LeaderPos>;
+
+    rclcpp_action::GoalResponse handle_goal(
+        const rclcpp_action::GoalUUID &,
+        std::shared_ptr<const swerm_msgs::action::LeaderPos::Goal> goal
+    );
+    rclcpp_action::CancelResponse handle_cancel(
+        const std::shared_ptr<GoalHandleLeaderPos> goal_handle
+    );
+    void handle_accepted(const std::shared_ptr<GoalHandleLeaderPos> goal_handle);
+    void execute(const std::shared_ptr<GoalHandleLeaderPos> goal_handle);
+
+    bool is_out_of_map(geometry_msgs::msg::PoseStamped start_pos, geometry_msgs::msg::PoseStamped goal_pos);
+
+    rclcpp_action::Server<swerm_msgs::action::LeaderPos>::SharedPtr action_server_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr leader_odom_publisher_;
+
+    std::unique_ptr<MppiSwermController> mppi_controller_;
+    std::vector<SwermState> swerm_states;
+
+    double publish_rate_ms;
+};
